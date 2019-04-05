6552ffa60302797e1fc07eddbf1712f13966e8b9
DinnerKang <jenhyuk0318@gmail.com>
2019-04-05 19:06:57 +0900

    repactory 마지막..

--- stack.js
+++ stack.js
@@ -1,51 +1,29 @@
 // /https://www.acmicpc.net/problem/10828
 var readline = require('readline'),
-    r1 = readline.createInterface(process.stdin, process.stdout),
-    input_number,
-    arr = [];
-
-function stackPush(data) {
-    arr.push(data);
-}
-
-function stackPop() {
-    arr.length == 0 ? console.log(-1) : console.log(arr.pop());
-}
-
-function stackSize() {
-    console.log(arr.length);
-}
-
-function stackEmpty() {
-    arr.length == 0 ? console.log(1) : console.log(0);
-}
-
-function stackTop() {
-    arr.length == 0 ? console.log(-1) : console.log(arr[arr.length - 1]);
-}
+    r1 = readline.createInterface(process.stdin, process.stdout);
 
 r1.question('반복할 횟수 : ', function (ans) {
-    input_number = Number(ans);
+    var input_number = Number(ans),
+        command,
+        arr = [];
+    r1.on('line', function (line) {
+        command = line.split(' ')[0];
+        if (command == 'push') {
+            arr.push((line.split(' ')[1]));
+        } else if (command == 'pop') {
+            arr.length == 0 ? console.log(-1) : console.log(arr.pop());
+        } else if (command == 'size') {
+            console.log(arr.length);
+        } else if (command == 'empty') {
+            arr.length == 0 ? console.log(1) : console.log(0);
+        } else if (command == 'top') {
+            arr.length == 0 ? console.log(-1) : console.log(arr[arr.length - 1]);
+        }
+        input_number--;
+        if (input_number == 0) r1.close();
+    });
+    r1.on('close', function () {
+        process.exit();
+    });
 });
 
-r1.on('line', function (line) {
-    var command = line.split(' ')[0];
-    if (command == 'push') {
-        stackPush(line.split(' ')[1]);
-    } else if (command == 'pop') {
-        stackPop();
-    } else if (command == 'size') {
-        stackSize();
-    } else if (command == 'empty') {
-        stackEmpty();
-    } else if (command == 'top') {
-        stackTop();
-    }
-    command = null;
-    input_number--;
-    if (input_number == 0) r1.close();
-});
-
-r1.on('close', function () {
-    process.exit();
-});
