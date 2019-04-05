78c1e63005f58e999a6e20788598d4db2503ee09
DinnerKang <jenhyuk0318@gmail.com>
2019-04-05 19:03:27 +0900

    repactory switch -> if

--- stack.js
+++ stack.js
@@ -5,61 +5,43 @@ var r1 = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-
-r1.on('line', function(line){
-    stackData(line);
-});
-
-r1.on('close', function(){
-    process.exit();
-});
-
-
-var input_number;
-var arr = [];
-
-
-function stackData(n){
-    if(input_number == null){
-        input_number = n;
-    }else{
-        var command = n.split(' ')[0];
-        var data = n.split(' ')[1];
-        
-        switch (command){
-            case 'push':
-                arr.push(data);
-                break;
-            case 'pop':
-                if(arr.length == 0 ){
-                    console.log(-1);
-                }else{
-                    console.log(arr.pop());
-                }
-                break;
-            case 'size':
-                console.log(arr.length);
-                break;
-            case 'empty':
-                if(arr.length ==0){
-                    console.log(1);
-                }else{
-                    console.log(0);
-                }
-                break;
-            case 'top':
-                if(arr.length == 0){
-                    console.log(-1);
-                }else{
-                    console.log(arr[arr.length -1]);
-                }
-                break;
+var input_number = null,
+    arr = [];
+r1.on('line', function (line) {
+    if (input_number == null) {
+        input_number = line;
+    } else {
+        var command = line.split(' ')[0];
+        var arr_len = arr.length;
+        if (command == 'push') {
+            arr.push(line.split(' ')[1]);
+        } else if (command == 'pop') {
+            if (arr_len == 0) {
+                console.log(-1);
+            } else {
+                console.log(arr.pop());
+            }
+        } else if (command == 'size') {
+            console.log(arr_len);
+        } else if (command == 'empty') {
+            if (arr_len == 0) {
+                console.log(1);
+            } else {
+                console.log(0);
+            }
+        } else if (command == 'top') {
+            if (arr_len == 0) {
+                console.log(-1);
+            } else {
+                console.log(arr[arr_len - 1]);
+            }
         }
         input_number--;
-        if(input_number == 0){
+        if (input_number == 0) {
             r1.close();
         }
     }
-}
-
+});
+r1.on('close', function () {
+    process.exit();
+});
