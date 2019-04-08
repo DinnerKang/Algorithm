e14e4630e40dee3ac4d2ffbd869aef140eb7b544
DinnerKang <jenhyuk0318@gmail.com>
2019-04-08 11:33:23 +0900

    백준용 디버그 제작

--- stack.js
+++ stack.js
@@ -27,3 +27,27 @@ r1.question('반복할 횟수 : ', function (ans) {
     });
 });
 
+// 백준 디버그용
+var fs = require('fs');
+var stdin = fs.readFileSync('/dev/stdin', 'utf8');
+var input = stdin.match(/[^\r\n]+/g);
+
+
+var input_number = parseInt(input[0], 10),
+    command,
+    arr = [];
+
+for (var i = 1; i <= input_number; i++) {
+    command = input[i].split(' ')[0];
+    if (command == 'push') {
+        arr.push((input[i].split(' ')[1]));
+    } else if (command == 'pop') {
+        arr.length == 0 ? console.log(-1) : console.log(arr.pop());
+    } else if (command == 'size') {
+        console.log(arr.length);
+    } else if (command == 'empty') {
+        arr.length == 0 ? console.log(1) : console.log(0);
+    } else if (command == 'top') {
+        arr.length == 0 ? console.log(-1) : console.log(arr[arr.length - 1]);
+    }
+}
