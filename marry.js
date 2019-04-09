4ac435c6a5673f61be56a8000053edacb343a50c
DinnerKang <jenhyuk0318@gmail.com>
2019-04-09 11:13:55 +0900

    백준 marry 성공

--- marry.js
+++ marry.js
@@ -8,14 +8,25 @@ r1.question('1번째 : ', function (ans) {
         var list = ans;
         var my_friend_list = [];
         var next_friend = [];
+        var hold_friend = [];
         r1.on('line', function (line) {
             if (line == 'exit') {
                 console.log(my_friend_list);
                 console.log(next_friend);
+                console.log(hold_friend);
+                for(var j=0; j<hold_friend.length; j++){
+                    if( my_friend_list.indexOf(hold_friend[j].split(' ')[0]) != -1){
+                        next_friend.push(hold_friend[j].split(' ')[1]);
+                    }else if(my_friend_list.indexOf(hold_friend[j].split(' ')[1]) != -1){
+                        next_friend.push(hold_friend[j].split(' ')[0]);
+                    }
+                }
 
                 // 합치고 중복값 제거
                 var result = my_friend_list.concat(next_friend).reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+                
                 // 1은 내 자신이니깐
+                console.log(result);
                 console.log('1 은 빼고 결과 :',result.length -1);
                 r1.close();
             }
@@ -30,6 +41,8 @@ r1.question('1번째 : ', function (ans) {
                 next_friend.push(b);
             }else if(my_friend_list.indexOf(b) != -1 ){
                 next_friend.push(a);
+            }else{
+                hold_friend.push(line);
             }
 
         });
